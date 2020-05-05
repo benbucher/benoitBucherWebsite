b4961fa5622104e33fbdde0a5ee59a67af264e0c
Benoit Bucher <benoit.bucher@yahoo.fr>
2020-05-05 19:42:13 +0200

    photo album grid generation through javascript is fanalized. The script os generic and the same script can therefore be called by each of the album pages. This is great! The grid can still be improved, especially the horizontal disposition. Margins could be all set to the left for instance. And flex along the main direciton could be set to sapce between to have enough margin whe two pics are on the same line for instance.

--- src/albumPhotoSize.js
+++ src/albumPhotoSize.js
@@ -1,66 +1,60 @@
 
-alert('Hello world!');
+/*
 
-// create array of all CSS elements
-var photoArray = ['album_001_photo_001', 'album_001_photo_002', 'album_001_photo_003', 'album_001_photo_004'];
+Author: Benoit Bucher
+Date: April 2020
 
-// go through the array
-for (var i = 0, c = photoArray.length; i < c; i++) {
-	
+Description:
+
+This script will be called by each html album page to generate the width and margin of the photo from the current album. This script is generic and can be called by all the album pages without changing anything in the script
+
+*/
+
+
+// retrieve the list of all the img tags in the container #albumPhotosContainer
+var imgTableList = document.querySelectorAll('#albumPhotosContainer img');
+
+// get the album number from the first pivture id name
+var albumNumber = imgTableList[0].id.slice(0, -3); // remove the last three characters containing the photo number
+
+// go through the list of pictures of the album
+for (var i = 0, c = imgTableList.length; i < c; i++) {
+
 	// draw random width
-	width = getRandomSize(20, 100);
+	width = getRandomSize(20, 95); // the width of the picture is between 20% and 95% of the container width
+
+	// TODO? add condition for portrait picture to draw the size between 20 and 70 for landscape screens
+
+	// TODO? consider the option to have the horizontal margin, only on the left and nothing on the right, to better center the pictures horizontally
+
 
 	// draw random margins
-	marginHorizontal = getRandomSize(10, width);
-	marginVertical = 24 * getRandomSize(1, 10);
-	horizontalSplit = getRandomSize(0, 10) / 10;
-	veticalSplit = getRandomSize(0, 10) / 10;
+	marginHorizontal = 0.8 * getRandomSize(0, (100 - width)); // the sum of the left and right margin shall be between 0% and (100%-width)
+	horizontalSplit = getRandomSize(2, 8) / 10; // draw the repartition between left and right margin while ensureing minimum margin on left and right max desequilibrium set to 0.2 vs 0.8
+	marginTop = 24 * getRandomSize(1, 6); 
+	marginBottom = 24 * getRandomSize(1, 6);
+
+	// rebuild id tag for each photo number of the album
+	var catAlbumAndPhotoNumber = eval(albumNumber + String(pad(i+1, 3)));
 
 	// apply width
-	eval(photoArray[i]).style.width = String(width) + '%';
+	catAlbumAndPhotoNumber.style.width = String(width) + '%';
 
 	// apply margins
-	eval(photoArray[i]).style.marginLeft = String(marginHorizontal * horizontalSplit) + '%';
-	eval(photoArray[i]).style.marginRight = String(marginHorizontal * (1 - horizontalSplit)) + '%';
-	eval(photoArray[i]).style.marginTop = String(marginVertical * veticalSplit) + 'px';
-	eval(photoArray[i]).style.marginBottom = String(marginVertical * (1 - veticalSplit)) + 'px';
+	catAlbumAndPhotoNumber.style.marginLeft = String(marginHorizontal * horizontalSplit) + '%';
+	catAlbumAndPhotoNumber.style.marginRight = String(marginHorizontal * (1 - horizontalSplit)) + '%';
+	catAlbumAndPhotoNumber.style.marginTop = String(marginTop) + 'px';
+	catAlbumAndPhotoNumber.style.marginBottom = String(marginBottom) + 'px';
 
-	//eval(photoArray[i]).style.marginLeft = '80px';
-	//eval(photoArray[i]).style.marginRight = '8px';
-	//eval(photoArray[i]).style.marginTop = '24px';
-	//eval(photoArray[i]).style.marginBottom = '40px';
-
-
-		
 }
 
 
-//alert(string(eval(getRandomSize(0.2, 1.0))));
-
-// computation method
-//width = calc(100% * getRandomSize(0.2, 1.0));
-
-
-// copied from web
-
+// random draw function
 function getRandomSize(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-//var allImages = "";
-
-//for (var i = 0; i < 25; i++) {
-//  var width = getRandomSize(200, 400);
-//  var height =  getRandomSize(200, 400);
-//  allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
-//}
-
-//$('#photos').append(allImages);
-
-
-
-
-
-
-//album_001_photo_002.style.width = '10%';
-//album_001_photo_002.style.marginBottom = '48px';
+// leading zero padding function
+function pad(num, size) {
+	return ('000000000' + num).substr(-size);
+}
