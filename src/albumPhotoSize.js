f09196212197e8e74c4ca0a6c5ec763083478034
Benoit Bucher <benoit.bucher@yahoo.fr>
2020-05-07 14:56:51 +0200

    first commit of package and json files. All files have been updated after prettifier run and linter run

--- src/albumPhotoSize.js
+++ src/albumPhotoSize.js
@@ -1,4 +1,3 @@
-
 /*
 
 Author: Benoit Bucher
@@ -20,41 +19,41 @@ var albumNumber = imgTableList[0].id.slice(0, -3); // remove the last three char
 // go through the list of pictures of the album
 for (var i = 0, c = imgTableList.length; i < c; i++) {
 
-	// draw random width
-	width = getRandomSize(20, 95); // the width of the picture is between 20% and 95% of the container width
+    // draw random width
+    width = getRandomSize(20, 95); // the width of the picture is between 20% and 95% of the container width
 
-	// TODO? add condition for portrait picture to draw the size between 20 and 70 for landscape screens
+    // TODO? add condition for portrait picture to draw the size between 20 and 70 for landscape screens
 
-	// TODO? consider the option to have the horizontal margin, only on the left and nothing on the right, to better center the pictures horizontally
+    // TODO? consider the option to have the horizontal margin, only on the left and nothing on the right, to better center the pictures horizontally
 
 
-	// draw random margins
-	marginHorizontal = 0.8 * getRandomSize(0, (100 - width)); // the sum of the left and right margin shall be between 0% and (100%-width)
-	horizontalSplit = getRandomSize(2, 8) / 10; // draw the repartition between left and right margin while ensureing minimum margin on left and right max desequilibrium set to 0.2 vs 0.8
-	marginTop = 24 * getRandomSize(1, 6); 
-	marginBottom = 24 * getRandomSize(1, 6);
+    // draw random margins
+    marginHorizontal = 0.8 * getRandomSize(0, (100 - width)); // the sum of the left and right margin shall be between 0% and (100%-width)
+    horizontalSplit = getRandomSize(2, 8) / 10; // draw the repartition between left and right margin while ensureing minimum margin on left and right max desequilibrium set to 0.2 vs 0.8
+    marginTop = 24 * getRandomSize(1, 6);
+    marginBottom = 24 * getRandomSize(1, 6);
 
-	// rebuild id tag for each photo number of the album
-	var catAlbumAndPhotoNumber = eval(albumNumber + String(pad(i+1, 3)));
+    // rebuild id tag for each photo number of the album
+    var catAlbumAndPhotoNumber = eval(albumNumber + String(pad(i + 1, 3)));
 
-	// apply width
-	catAlbumAndPhotoNumber.style.width = String(width) + '%';
+    // apply width
+    catAlbumAndPhotoNumber.style.width = String(width) + '%';
 
-	// apply margins
-	catAlbumAndPhotoNumber.style.marginLeft = String(marginHorizontal * horizontalSplit) + '%';
-	catAlbumAndPhotoNumber.style.marginRight = String(marginHorizontal * (1 - horizontalSplit)) + '%';
-	catAlbumAndPhotoNumber.style.marginTop = String(marginTop) + 'px';
-	catAlbumAndPhotoNumber.style.marginBottom = String(marginBottom) + 'px';
+    // apply margins
+    catAlbumAndPhotoNumber.style.marginLeft = String(marginHorizontal * horizontalSplit) + '%';
+    catAlbumAndPhotoNumber.style.marginRight = String(marginHorizontal * (1 - horizontalSplit)) + '%';
+    catAlbumAndPhotoNumber.style.marginTop = String(marginTop) + 'px';
+    catAlbumAndPhotoNumber.style.marginBottom = String(marginBottom) + 'px';
 
 }
 
 
 // random draw function
 function getRandomSize(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 // leading zero padding function
 function pad(num, size) {
-	return ('000000000' + num).substr(-size);
+    return ('000000000' + num).substr(-size);
 }
