73e7820e83147b06570831a1fa9936bfed65f75a
Benoit Bucher <benoit.bucher@yahoo.fr>
2020-05-15 00:36:07 +0200

    responsivity for the tablet version has been finalized. Lightbow margins have been sovled

--- src/lightbox.js
+++ src/lightbox.js
@@ -37,7 +37,7 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "flex";
 }
 
 
